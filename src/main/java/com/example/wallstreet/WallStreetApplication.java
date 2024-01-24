a30ac17746ec65c199a8325df79fdab72c5fb567
medertynychbekov <meder.tynychbekov@gmail.com>
2024-01-24 15:17:24 +0300

    init

---
+++ src/main/java/com/example/wallstreet/WallStreetApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallstreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallStreetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallStreetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wallstreet/WallStreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallstreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallStreetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
